beb660c7ff48a275730b2ae292e069ee8e9e3680
rachail Pomerlee <rachailpomerlee@rachails-MacBook-Pro.local>
2023-11-29 13:28:28 -0500

    Boilerplate, wireframe, and psuedocode was added.

---
+++ README.md
@@ -0,0 +1,1 @@
+![Wireframe](https://docs.google.com/document/d/1NEJirq_1tAGX4bgbZU9lpAQDNczXInBE1Tre5MjBPa0/edit?usp=drive_link)
---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*----- constants -----*/
+
+
+	/*----- state variables -----*/
+
+
+	/*----- cached elements  -----*/
+
+
+	/*----- event listeners -----*/
+
+
+	/*----- functions -----*/
+    //psuedocode
+    //state variables
+    //board: 2d array
+    //0 = no fliped cards
+    //1 = 1 flipped card
+    //-1 = 2 flipped card
+    //pair match: null = no match
+    //2 = match
