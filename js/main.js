1cdf10f30cb4d65310ab914202f525de5fffb1f8
rachail Pomerlee <rachailpomerlee@rachails-MacBook-Pro.local>
2023-12-09 10:16:10 -0500

    I changed the div, add some function and event listeners for clicks

--- js/main.js
+++ js/main.js
@@ -1,19 +1,54 @@
-/*----- constants -----*/
+ /*----- state variables -----*/
+    let firstCard, secondCard;
+    let picked = false;
+    let lockTable = false;
+   
+    
+    const deck = document.querySelectorAll('.card');
 
+    function cardFlip() {
+        if (lockTable)return;
+        this.classList.add('flip');
+        if (!picked) {
+            picked = true;
+            firstCard = this;
+        } else {
+            picked = false;
+            secondCard = this;
 
-	/*----- state variables -----*/
+            checkIfMatch()
+        }
+    }
 
+function checkIfMatch() {
+  if (firstCard.dataset.name === secondCard.dataset.name) {
+    keepFliped();
+  } else {
+    unflip();
+  }
+}
 
-	/*----- cached elements  -----*/
+function keepFliped() {
+  firstCard.removeEventListener('click', cardFlip);
+  secondCard.removeEventListener('click', cardFlip);
+}
 
+function unflip() {
+    lockTable = true;
 
-	/*----- event listeners -----*/
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
 
+    lockTable = false;
+}, 1500);
+}
 
-	/*----- functions -----*/
+deck.forEach(card => card.addEventListener('click', cardFlip))
+
     //psuedocode
     //state variables
-    //board: 2d array
+    //board: array
     //0 = no fliped cards
     //1 = 1 flipped card
     //-1 = 2 flipped card
